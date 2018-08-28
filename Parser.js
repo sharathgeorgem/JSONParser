481f0189824cc059f4d7c7f4615d08043f0b14eb
Sharath George M <sharathgeorgem@gmail.com>
2018-08-28 12:35:53 +0530

    Update Parser.js

--- Parser.js
+++ Parser.js
@@ -1,14 +1,14 @@
-var tokens = [];
-
 function parse(str) {
     
 }
 
+//Null Parser
 function parseNull(data) {
     if(data.startsWith('null'))
         return [null, data.slice(4)];
 }
 
+//Boolean Parser
 function parseBool(data) {
     if(data.startsWith('true'))
         return [true, data.slice(4)];
@@ -16,16 +16,18 @@ function parseBool(data) {
         return [false, data.slice(5)];
 }
 
+//Number Parser
 function parseNumber(data) {
-    var check = /^\d+/;
+    var check = /[+-]?[0-9]*\.?[0-9]+(?:[Ee][+-]?[0-9]+)?/;
     if(data.match(check)) {
         var num = data.match(check);
         return [Number(num[0]), data.slice(num[0].length)];
     }    
 }
 
+//String Parser
 function parseString(data) {
 
 }
 
-console.log(parseNumber('12345truefalsenull'));
+console.log(parseNumber('+4.HelloThere'));
