48ff3c0b7cefb356103d5fc8d143ce98b4f693ee
Sharath George M <sharathgeorgem@gmail.com>
2018-08-29 09:01:07 +0530

    Number and string parser
    Number and string parsers using RegEx.

--- Parser.js
+++ Parser.js
@@ -1,9 +1,10 @@
+// Null parser
 function parseNull (data) {
   if (data.startsWith('null')) {
     return [null, data.slice(4)]
   }
 }
-
+// Boolean parser
 function parseBool (data) {
   if (data.startsWith('true')) {
     return [true, data.slice(4)]
@@ -11,7 +12,7 @@ function parseBool (data) {
     return [false, data.slice(5)]
   }
 }
-
+// Number parser
 function parseNumber (data) {
   let check = /[+-]?[0-9]*\.?[0-9]+(?:[Ee][+-]?[0-9]+)?/
   let num = data.match(check)
@@ -19,19 +20,27 @@ function parseNumber (data) {
     return [Number(num[0]), data.slice(num[0].length)]
   }
 }
-
+// String parser
 function parseString (data) {
   if (data.startsWith('""')) {
     return ['', data.slice(2)]
   } else if (data.startsWith('"')) {
-    let check = /"((\\["\\/\\b\\f\\n\\r\\t\\u])*[^\u005C"]+)+((\\["\\/\\b\\f\\n\\r\\t\\u])*[^\u005C"]*)*"/
+    let check = /"((\\["\\/\\b\\f\\n\\r\\t\\u[0-9A-Fa-f]{4}])*[^\u005C"]+)+((\\["\\/\\b\\f\\n\\r\\t\\u[0-9A-Fa-f]{4}])*[^\u005C"]*)*"/
     let num = data.match(check)
-    console.log(num)
     if (num) {
-      return [String(num[0]), data.slice(num[0].length)]
+      return [num[0], data.slice(num[0].length)]
     }
   }
 }
 
-console.log(parseString('"\nmanchester\n\tunited\t"123456'))
+console.log(parseString('"\n\tHello World\t\n"123456'))
+console.log(parseBool('trueWaddup'))
+console.log(parseNull('nullNothin\'much'))
+console.log(parseNumber('123456HelloWorl\'d!'))
+
+/*function parseArray (data) {
+  if (data.startsWith('[')) {
+    
+  }
+}*/
 
